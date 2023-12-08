d7636c05afa239a5ea2fb2ff2bf475684abec637
Rebeca Primo <rebecarodriguesprimo036@gmail.com>
2023-12-08 16:39:22 -0300

    fix: teste vercel

--- react-firebase/src/firebase.js
+++ react-firebase/src/firebase.js
@@ -3,15 +3,7 @@ import { getAuth } from "firebase/auth";
 import { getFirestore } from "firebase/firestore";
 import { getStorage } from "firebase/storage";
 
-const firebaseConfig = {
-  apiKey: "AIzaSyAdnEuN_QQAkHG994xyy2HmTJbeaUAPvlk",
-  authDomain: "expense-tracker-36293.firebaseapp.com",
-  projectId: "expense-tracker-36293",
-  storageBucket: "expense-tracker-36293.appspot.com",
-  messagingSenderId: "107310237329",
-  appId: "1:107310237329:web:045bccf326afb549df6130",
-  measurementId: "G-Q062VWCTP0"
-};
+const firebaseConfig = process.env.REACT_APP_FIREREBASE;
 
 const app = initializeApp(firebaseConfig);
 export const db = getFirestore(app);
